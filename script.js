6dc565dd2c1e58e74faab8133593c1a65864de68
Disvelop <disvelop@gmail.com>
2021-08-25 17:19:02 -0600

    update1

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("animp").addEventListener("click", animProfile)
+
+function animProfile(){
+	window.open("animator.html","_self")
+}
+function scrpProfile(){
+	window.open("scripter.html","_self")
+}
