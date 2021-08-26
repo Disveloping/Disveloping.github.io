bee3d3db1c9b4a39777c0180edaadbbaf704f4cb
Disvelop <disvelop@gmail.com>
2021-08-26 13:14:30 -0600

    Added first examples

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById("animp").addEventListener("click", animProfile)
+document.getElementById("scrp").addEventListener("click", scrpProfile)
 
 function animProfile(){
+	console.log("animating on")
 	window.open("animator.html","_self")
 }
 function scrpProfile(){
+	console.log("scripting on")
 	window.open("scripter.html","_self")
 }
